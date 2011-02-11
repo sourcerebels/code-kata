faf72e0d4ed12e5556430b6945af3657478daeca
Edu Rodriguez <edu@sourcerebels.com>
2011-02-12 00:21:14 +0100

    Added some python docs

--- thecube/python/Thecube.py
+++ thecube/python/Thecube.py
@@ -1,6 +1,7 @@
 class Thecube():
 
     def addDigits(self, text):
+        """Returns the digit's sum of given text"""
         result = 0
         for pos in range(0, len(text)):
             if text[pos].isdigit():
@@ -8,12 +9,14 @@ class Thecube():
         return result
 
     def isPrimeNumber(self, number):
+        """Determines if a number is prime"""
         for previousNumber in range(number - 1, 1, -1):
             if number % previousNumber == 0:
                 return False
         return True
 
     def containsPrimeNumber(self, code1, code2, code3):
+        """The Cube kata"""
         if (self.isPrimeNumber(self.addDigits(code1))):
             return True
         if (self.isPrimeNumber(self.addDigits(code2))):
