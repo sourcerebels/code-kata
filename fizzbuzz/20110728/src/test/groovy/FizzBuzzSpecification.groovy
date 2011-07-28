588d4296e039631356676636f9457460b0636cff
Edu Rodríguez Castillo <edu@sourcerebels.com>
2011-07-28 22:17:49 +0200

    ROJO: Añadido primer test

--- fizzbuzz/20110728/src/test/groovy/FizzBuzzSpecification.groovy
+++ fizzbuzz/20110728/src/test/groovy/FizzBuzzSpecification.groovy
@@ -1,4 +1,18 @@
+import spock.lang.*
+
+
+class FizzBuzz {
+	
+	static answer(def number) {
+		return null
+	}
+}
+
 class FizzBuzzSpecification extends Specification {
 
+	def "deberia responder el mismo si 1"() {
+		expect:
+			FizzBuzz.answer(1) == "1"			 	
+	}
 
 }
