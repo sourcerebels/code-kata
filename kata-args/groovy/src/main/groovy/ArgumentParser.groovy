8d29ba26233ae7da9409abf46f4873bc14f2867c
Edu Rodríguez Castillo <edu@sourcerebels.com>
2011-08-01 18:03:10 +0200

    Green. Refactor needed

--- kata-args/groovy/src/main/groovy/ArgumentParser.groovy
+++ kata-args/groovy/src/main/groovy/ArgumentParser.groovy
@@ -1,7 +1,6 @@
 class ArgumentParser {
 
 	final static SCHEMA_SEPARATOR = " "
-	final static FLAG_INDICATOR = "-"
 		
 	def schema
 	def arguments
@@ -42,32 +41,15 @@ class ArgumentParser {
 		
 		Argument argument = new Argument()
 		argument.parseSchemaDefinition(argumentDefinition)
-		argument.value = getArgumentValue(argument.flag, args)
+		argument.parseArgumentValue(args)
 		addArgument(argument)
 	}
-
+	
 	private addArgument(def argument) {
 		
 		arguments.put(argument.flag, argument)
 	}
 
-	private isFlag(def argument, def flag) {
-		
-		return (argument == FLAG_INDICATOR + flag)
-	}
-	
-	private getArgumentValue(def flag, def args) {
-		
-		for (def i = 0; i < args.size(); i++) {
-			
-			if(isFlag(args[i], flag)) {
-				
-				return args[i + 1]
-			}
-		}
-		return null
-	}
-		
 	private schemaAsList() {
 
 		return this.schema.split(SCHEMA_SEPARATOR)
--- kata-args/groovy/src/test/groovy/KataArgsTestSpec.groovy
+++ kata-args/groovy/src/test/groovy/KataArgsTestSpec.groovy
@@ -36,4 +36,21 @@ class KataArgsTestSpec extends Specification {
 			"sv sb" | ["-v", "value", "-b","bcn"] | "b"  | "bcn"
 			"sb sv" | ["-v", "value", "-b","bcn"] | "b"  | "bcn"
 	}
+
+
+	@Unroll
+	def "should parse a simple boolean value"() {
+		
+		setup:
+			parser = new ArgumentParser(schema: schema)
+			parser.parse(args)
+		expect:
+			parser.argumentNumber == 1
+			parser.getArgumentValue(flag) == result
+		where: "different schemas, two strings, arguments are unsorted"
+			schema  | args   | flag | result
+			"bl"    | ["-l"] | "l"  | true
+			"bl"    | [""]   | "l"  | false
+	}
+
 }
